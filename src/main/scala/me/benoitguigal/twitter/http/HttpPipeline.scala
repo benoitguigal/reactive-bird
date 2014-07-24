4a82df2e5784679870a1f6f3bc0d0f929beda513
benoit <benoit.guigal@gmail.com>
2014-07-24 17:44:46 +0200

    sendReceive is evaluated lazily

--- src/main/scala/me/benoitguigal/twitter/http/HttpPipeline.scala
+++ src/main/scala/me/benoitguigal/twitter/http/HttpPipeline.scala
@@ -12,25 +12,25 @@ import me.benoitguigal.twitter.oauth.OAuth._
 import me.benoitguigal.twitter.TwitterApi
 import me.benoitguigal.twitter.host
 import scala.concurrent.Future
+import spray.http.HttpRequest
+import me.benoitguigal.twitter.oauth.Consumer
+import me.benoitguigal.twitter.oauth.Token
 
 object HttpPipeline {
 
   import TwitterApi.{system, exec, timeout}
 
-  def apply(consumer: Consumer, token: Token): Future[SendReceive] = for (
+  lazy private val sendReceiveFut = for (
     Http.HostConnectorInfo(connector, _) <-
     IO(Http) ? Http.HostConnectorSetup(host, port = 443, sslEncryption = true)
-  ) yield (
-      { request: HttpRequest => request.authorize(consumer, token) }
-        ~> sendReceive(connector)
-        ~> errorFilter)
+  ) yield (sendReceive(connector))
 
-  def apply(consumer: Consumer, oauthCallback: String): Future[SendReceive] = for (
-    Http.HostConnectorInfo(connector, _) <-
-    IO(Http) ? Http.HostConnectorSetup(host, port = 443, sslEncryption = true)
-  ) yield (
-      { request: HttpRequest => request.authorize(consumer, oauthCallback) }
-        ~> sendReceive(connector)
-        ~> errorFilter)
+  def apply(consumer: Consumer, token: Token): Future[SendReceive] = sendReceiveFut map { sendReceive =>
+    { request: HttpRequest => request.authorize(consumer, token) } ~> sendReceive ~> errorFilter
+  }
+
+  def apply(consumer: Consumer, oauthCallback: String) = sendReceiveFut map { sendReceive =>
+    { request: HttpRequest => request.authorize(consumer, oauthCallback) } ~> sendReceive ~> errorFilter
+  }
 
 }
